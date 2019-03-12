2ac3a1c0f709b7f0075c123abc61737c729270e5
Ahmed Abbas <ahmed.abbas@zalando.de>
2019-03-12 22:22:49 +0100

    add caching for external assets & api network requests

--- src/sw.js
+++ src/sw.js
@@ -4,8 +4,23 @@ console.log("in sw.js");
 workbox.skipWaiting(); //activate new sw, when its known on reload
 workbox.clientsClaim(); //replace an existing active sw without waiting for reload
 
+//added to precache cache
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
 
+//cache external resources
+workbox.routing.registerRoute(
+    new RegExp('https?://.*min\.(css|js)'),
+    workbox.strategoes.networkFirst() //added to runtime cache
+)
+
+//cache json endpoint
+workbox.routing.registerRoute(
+    new RegExp('https?://.*:4567.*\.json'),
+    new RegExp('/.*\.json'),
+    workbox.strategoes.networkFirst({
+        cacheName: 'json-cache'
+    })
+)
 
 self.addEventListener('install', event => {
     console.log('sw install...');
