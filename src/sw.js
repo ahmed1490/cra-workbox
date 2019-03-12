211c1dc9aa9029ed814a972a783ceee57a550949
Ahmed Abbas <ahmed.abbas@zalando.de>
2019-03-12 22:02:51 +0100

    Added precaching for assets outputted by webpack

--- src/sw.js
+++ src/sw.js
@@ -3,3 +3,5 @@ console.log("in sw.js");
 //default, new a new sw replaced on a fresh visit of website (and hard reload sometimes)
 workbox.skipWaiting(); //activate new sw, when its known on reload
 workbox.clientsClaim(); //replace an existing active sw without waiting for reload
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
